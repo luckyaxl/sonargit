63d0102053ab044f5c6132dbef5bb4c740927cda
luckyaxl <luckyaxl3@gmail.com>
2023-12-02 05:17:27 +0700

    feat: add trycatch

--- src/core/sequentialProcess.ts
+++ src/core/sequentialProcess.ts
@@ -11,69 +11,65 @@ export const sequentialProcess = async (
   outputDir: string,
   logFilePath: string
 ) => {
-  const browser = await puppeteer.launch({
-    headless: "new",
-  });
-  const page = await browser.newPage();
+  try {
+    const browser = await puppeteer.launch({
+      headless: "new",
+    });
+    const page = await browser.newPage();
 
-  await page.setViewport({ width: 1512, height: 850 });
+    await page.setViewport({ width: 1512, height: 850 });
 
-  await page.goto(`${env.SONARQUBE_URL}/sessions/new?return_to=%2F`, {
-    waitUntil: "networkidle0",
-  });
+    await page.goto(`${env.SONARQUBE_URL}/sessions/new?return_to=%2F`, {
+      waitUntil: "networkidle0",
+    });
 
-  await page.type("#login", env.SONAR_LOGIN as string);
-  await page.type("#password", env.SONAR_PASSWORD as string);
+    await page.type("#login", env.SONAR_LOGIN as string);
+    await page.type("#password", env.SONAR_PASSWORD as string);
 
-  await page.keyboard.press("Enter");
+    await page.keyboard.press("Enter");
 
-  await page.waitForNavigation({
-    waitUntil: "networkidle0",
-  });
+    await page.waitForNavigation({
+      waitUntil: "networkidle0",
+    });
 
-  for (const item of items) {
-    try {
-      const comments = await fetchRegularComments(item.number);
+    for (const item of items) {
+      try {
+        const comments = await fetchRegularComments(item.number);
 
-      const percentage = comments?.percentage;
-      const sonarQubeUrl = comments?.sonarQubeUrl;
+        const percentage = comments?.percentage;
+        const sonarQubeUrl = comments?.sonarQubeUrl;
 
-      if (sonarQubeUrl) {
-        await page.goto(sonarQubeUrl, {
-          waitUntil: "networkidle0",
-        });
-        await page.screenshot({
-          path: `${outputDir}/screenshot_pr_${item.number}_${env.REPO}.png`,
-        });
-      }
-
-      const msg = `${moment
-        .utc(item.pull_request.merged_at)
-        .utcOffset(7)
-        .format("DD MMM YYYY")}, ${item.html_url}, ${percentage}`;
-
-      fs.appendFile(logFilePath, `${msg}\n`, (err) => {
-        if (err) {
-          console.error(
-            `Error writing to log file:`,
-            err
-          );
+        if (sonarQubeUrl) {
+          await page.goto(sonarQubeUrl, {
+            waitUntil: "networkidle0",
+          });
+          await page.screenshot({
+            path: `${outputDir}/screenshot_pr_${item.number}_${env.REPO}.png`,
+          });
         }
-      });
 
-      console.log(`${successColorAnsi("[+]")} ${msg}`);
-    } catch (error) {
-      console.error(
-        `Error processing pull request:`,
-        error
-      );
+        const msg = `${moment
+          .utc(item.pull_request.merged_at)
+          .utcOffset(7)
+          .format("DD MMM YYYY")}, ${item.html_url}, ${percentage}`;
+
+        fs.appendFile(logFilePath, `${msg}\n`, (err) => {
+          if (err) {
+            console.error(`Error writing to log file:`, err);
+          }
+        });
+
+        console.log(`${successColorAnsi("[+]")} ${msg}`);
+      } catch (error) {
+        console.error(`Error processing pull request:`, error);
+      }
     }
+
+    console.log(`\nDone. see results under ${outputDir}`);
+
+    await browser.close();
+    process.exit(1);
+  } catch (error) {
+    console.error(`Error:`, error);
   }
-
-  console.log(
-    `\nDone. see results under ${outputDir}`
-  );
-
-  await browser.close();
-  process.exit(1);
 };
