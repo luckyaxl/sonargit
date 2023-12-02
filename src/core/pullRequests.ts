fe622a8df91ed342b79bf2159095c66b94dab42b
luckyaxl <luckyaxl3@gmail.com>
2023-12-02 19:32:07 +0700

    feat: add while statement

--- src/core/pullRequests.ts
+++ src/core/pullRequests.ts
@@ -13,53 +13,75 @@ interface FetchPullRequests {
   logFilePath: string;
 }
 
+interface GitHubApiResponse {
+  total_count: number;
+  items: any[];
+}
+
 export const fetchPullRequests = async ({
   startDate,
   endDate,
   outputDirectory,
   logFilePath,
 }: FetchPullRequests) => {
-  const queryString: string = `is:pr is:closed merged:${startDate}..${endDate} base:${env.BASE_BRANCH} sort:updated-asc author:${env.AUTHOR} repo:${env.REPO} user:${env.OWNER}`;
+  console.log(
+    `${successColorAnsi("[*]")} scraping from repo: ${env.OWNER}/${
+      env.REPO
+    }, author: ${env.AUTHOR}`
+  );
 
-  const apiUrl: string = `https://api.github.com/search/issues?q=${encodeURIComponent(
-    queryString
-  )}&page=${env.PAGE}&per_page=${env.PER_PAGE}`;
+  let page = 1;
+  let hasNext = true;
+  let totalData = 0;
+  let data: GitHubApiResponse["items"] = [];
 
-  console.log(`${successColorAnsi("[*]")} scraping from repo: ${env.OWNER}/${env.REPO}, author: ${env.AUTHOR}`)
+  while (hasNext) {
+    const queryString: string = `is:pr is:closed merged:${startDate}..${endDate} base:${env.BASE_BRANCH} sort:updated-asc author:${env.AUTHOR} repo:${env.REPO} user:${env.OWNER}`;
 
-  try {
-    const response = await fetch(apiUrl, {
-      headers: {
-        Authorization: `Bearer ${env.GITHUB_TOKEN}`,
-        Accept: "application/vnd.github+json",
-      },
-    });
+    const apiUrl: string = `https://api.github.com/search/issues?q=${encodeURIComponent(
+      queryString
+    )}&page=${page}&per_page=100`;
 
-    if (!response.ok) {
-      throw new Error(`Error: ${response.status} - ${response.statusText}`);
-    }
+    try {
+      const response = await fetch(apiUrl, {
+        headers: {
+          Authorization: `Bearer ${env.GITHUB_TOKEN}`,
+          Accept: "application/vnd.github+json",
+        },
+      });
 
-    const data = await response.json();
-
-    if (data) {
-      console.log(
-        `${successColorAnsi("[*]")} extracting ${
-          data.total_count
-        } pull request from ${moment(startDate).format("DD MMM YY")} - ${moment(
-          endDate
-        ).format("DD MMM YY")}\n`
-      );
-
-      if (!fs.existsSync(outputDirectory)) {
-        fs.mkdirSync(outputDirectory, { recursive: true });
+      if (!response.ok) {
+        throw new Error(`Error: ${response.status} - ${response.statusText}`);
       }
 
-      sequentialProcess(data.items, outputDirectory, logFilePath);
+      const responseData: GitHubApiResponse = await response.json();
+
+      data = [...data, ...responseData.items];
+      totalData = responseData.total_count;
+
+      page += 1;
+
+      hasNext = data.length < responseData.total_count;
+    } catch (error: any) {
+      console.error(
+        `${errorColorAnsi("[!]")} Error fetching data from GitHub API:`,
+        error.message
+      );
+      process.exit(1);
     }
-  } catch (error: any) {
-    console.error(
-      `${errorColorAnsi("[!]")} Error fetching data from GitHub API:`,
-      error.message
-    );
   }
+
+  console.log(
+    `${successColorAnsi(
+      "[*]"
+    )} extracting ${totalData} pull request from ${moment(startDate).format(
+      "DD MMM YY"
+    )} - ${moment(endDate).format("DD MMM YY")}\n`
+  );
+
+  if (!fs.existsSync(outputDirectory)) {
+    fs.mkdirSync(outputDirectory, { recursive: true });
+  }
+
+  sequentialProcess(data, outputDirectory, logFilePath);
 };
