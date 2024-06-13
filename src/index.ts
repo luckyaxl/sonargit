20b58e3312bbea6e4757d98feb8f6a5bbe09279d
luckyaxl <luckyaxl3@gmail.com>
2024-06-13 16:58:21 +0700

    feat: default percentage value

--- src/index.ts
+++ src/index.ts
@@ -33,7 +33,7 @@ const validateFormat = (value: string) => {
   return value;
 };
 
-const banner = `${successColorAnsi("SonarGit v0.0.11")}
+const banner = `${successColorAnsi("SonarGit v0.0.12")}
 Automated bot scraper to streamline data extraction from GitHub pull requests
 and capture dynamic SonarQube screenshots.\n`;
 
